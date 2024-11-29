7e90f1c8045e27ff045f8121a46bbdbf76edc400
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-29 16:40:38 +0200

    Added filter to Favourites page

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,7 +1,9 @@
+import HomePageFilter from "../components/dynamic/HomePageFilter"
 export default function FavouritesPage(){
     return(
         <>
             <h1>Favorites page</h1>
+            <HomePageFilter />
         </>
     )
 }
